5bbe550113b15a670c75bcd41b0fe7f705a19376
heroin0 <black.phoenix4466@gmail.com>
2016-04-01 17:48:03 +0500

    Added Drop into TurnParser
    Finished Board(probably)
    Some tweaks in Game constructor

--- Hanabi/Program.cs
+++ Hanabi/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -38,13 +39,13 @@ namespace Hanabi
                 case ('B'): CardColor = Color.Blue; break;
                 case ('Y'): CardColor = Color.Yellow; break;
                 case ('W'): CardColor = Color.White; break;
-                    
+
                 default: CardColor = Color.Empty; break;//or throw some exception
             }
 
             CardCost = (uint)char.GetNumericValue(cardInfo[1]);
         }
-        
+
 
     }
 
@@ -64,8 +65,16 @@ namespace Hanabi
 
         public Card getCard()
         {
+            if (!IsEmpty())
             return deck.Dequeue();
+            return new Card();
             //catch if empty?
+        }
+
+        public bool IsEmpty()
+        {
+            if (deck.Count() > 0) return false;
+            return true;
         }
     }
 
@@ -83,83 +92,163 @@ namespace Hanabi
             Hand.Add(newCard);
         }
 
-        public Card Play(int position,Card topDeck)
+        void RemoveCard(int position)
+        {
+            Hand.RemoveAt(position);
+        }
+
+        public Card Play(int position, Card topDeck)//шта эта делает? переименовать чутка
         {
             Card CardToPlay = Hand[position];
-            Hand.RemoveAt(position);
-            if (topDeck.CardCost!=0)
-            Hand.Add(topDeck);
+            RemoveCard(position);
+            if (topDeck.CardCost != 0)//подумать
+                Take(topDeck);
             return (CardToPlay);
         }
 
-        //bool Tell(int[] costs)
+        public void Drop(int position, Card topDeck)//TODO - think about connecting this and prev method
+        {
+            RemoveCard(position);
+            if (topDeck.CardCost != 0)
+                Take(topDeck);
+        }
+
+
+        //public bool Tell(uint cost, int[] positions)
         //{
 
         //}
 
-        //bool Tell()
+        //public bool Tell(string color, int[] positions)
 
     }
 
+    class Board
+    {
+        Dictionary<Color,uint> CardsOnBoard;
+        Board()
+        {
+            CardsOnBoard = new Dictionary<Color, uint>();
+        }
 
-    public class Game//TODO: Read about metaclasses in C#
+        bool AddCard(Card CardToAdd)
+        {
+            if (CardToAdd.CardCost == CardsOnBoard[CardToAdd.CardColor])
+            {
+                CardsOnBoard[CardToAdd.CardColor] += 1;
+                if (CardsOnBoard[CardToAdd.CardColor] == 5)
+                {
+                    CardsOnBoard[CardToAdd.CardColor] = 0;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        uint Cost()
+        {
+            uint SummaryCardsCost=0;
+            foreach(var Card in CardsOnBoard)
+            {
+                SummaryCardsCost += Card.Value;
+            }
+            return SummaryCardsCost;
+         }
+    }
+
+    public class HanabiGame//TODO: Read about metaclasses in C#
     {
         uint Turns;
         uint CardsPlayed;
         uint Riscs;
-        ushort ActivePlayer;
+        ushort ActivePlayerNumber, InactivePlayerNumber;//TODO: заменить на указатели, менять указатели
         Deck GameDeck;
         Player[] Players;
 
-       public Game()//TODO :divide class initialization and string logics(AKA turn parser)
+        public HanabiGame()//TODO :divide class initialization and string logics(AKA turn parser)
         {
             Turns = 0;
             CardsPlayed = 0;
             Riscs = 0;
-            ActivePlayer = 1;
+            ActivePlayerNumber = 0;
+            InactivePlayerNumber = 1;
             GameDeck = new Deck();
             Players = new Player[2];//TODO - написать конструктор для массива плееров?
             Players[0] = new Player();
             Players[1] = new Player();
             string inputString = "Start new game with deck R1 G2 B3 W4 Y5 R1 R1 B1 B2 W1 W2 W1";//Console.ReadLine();
             startParser(inputString);
-            
-
             inputString = Console.ReadLine();
             while (inputString != null)
             {
+                
                 turnParser(inputString);
-                inputString = Console.ReadLine();
+                Turns++;//если ход корректный
+
+                var temp = ActivePlayerNumber;
+                ActivePlayerNumber = InactivePlayerNumber;
+                InactivePlayerNumber = temp;
+                inputString = Console.ReadLine();//подумать, что делать, если игра окончена.
             }
-            Console.WriteLine("Turn: " + Turns + ", cards: " + CardsPlayed + ", with risk: " + Riscs);
+            Console.WriteLine("Turn:{0}, cards: {1}, with risk: {2}", Turns, CardsPlayed,Riscs);//TODO - проверить корректность после переработки
         }
 
-        void startParser(string Line)
+        void startParser(string line)
         {
-            var cards = Line.Split(' ').Skip(5).ToArray();
+            var cards = line.Split(' ').Skip(5).ToArray();
             for (var i = 0; i < cards.Count(); i++)
             {
                 Card topDeck = new Card(cards[i]);
                 if (i < 5)
                     Players[0].Take(topDeck);
-                else if (i >= 5 && i < 10)//TODO check this out
+                else if (i >= 5 && i < 10)//handsize, handsize*5
                     Players[1].Take(topDeck);
                 else
                     GameDeck.addCard(topDeck);
             }
         }
-        
+
         void turnParser(string line)
         {
-            
+            var action = line.Split(' ');
+            switch (action[0])
+            {
+                case "Play":
+                    {
+                        //do sth
+                    }
+                    break;
+                case "Drop":
+                    {
+                        //TODO:сделать проверку, а есть ли на этой позиции вообще карта
+                        var topDeck = GameDeck.getCard();
+                        Players[ActivePlayerNumber].Drop(Int32.Parse(action[2]), topDeck);
+                    }
+                    break;
+
+                case "Tell":
+                    {
+                        if (action[1] == "color")
+                        {
+
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    break;
+            }
         }
+
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Game newGame = new Game();
+     
+            HanabiGame newGame = new HanabiGame();
         }
     }
 }
